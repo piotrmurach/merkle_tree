a44129671ee9541a8a05b359b6d86e01b7748454
Piotr Murach <pmurach@gmail.com>
2021-03-20 14:10:58 +0100

    Add test to ensure tree leaves ordering

--- spec/unit/leaf/eql_spec.rb
+++ spec/unit/leaf/eql_spec.rb
@@ -6,11 +6,22 @@ RSpec.describe MerkleTree::Leaf, "#==" do
     leaf_b = MerkleTree::Leaf.build("L1", 1)
 
     expect(leaf_a).to_not eq(leaf_b)
+    expect(leaf_a).to be < leaf_b
   end
 
   it "compares successfully only with the same leaf" do
     leaf = MerkleTree::Leaf.build("L1", 0)
 
     expect(leaf).to eq(leaf)
+  end
+
+  it "orders leaves based on their values" do
+    leaf_a = MerkleTree::Leaf.build("L1", 1)
+    leaf_b = MerkleTree::Leaf.build("L2", 2)
+    leaf_c = MerkleTree::Leaf.build("L3", 3)
+
+    expect(leaf_a).to be < leaf_b
+    expect(leaf_b).to be < leaf_c
+    expect(leaf_b).to be_between(leaf_a, leaf_c)
   end
 end
