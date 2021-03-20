95736dfd540235bc60c7b5c25ba0c019cf33a151
Piotr Murach <pmurach@gmail.com>
2021-03-20 12:53:42 +0100

    Add performance tests for memory allocation of a new tree

--- spec/perf/speed_spec.rb
+++ spec/perf/speed_spec.rb
@@ -5,26 +5,42 @@ require "rspec-benchmark"
 RSpec.describe "speed performance" do
   include RSpec::Benchmark::Matchers
 
-  it "creates merkle trees in linear time" do
-    messages = bench_range(8, 8 << 12).map do |n|
-      Array.new(n) { "L#{n}" }
+  context "#new" do
+    it "creates merkle trees in linear time" do
+      sizes = bench_range(8, 8 << 12)
+      messages = sizes.map { |n| Array.new(n) { "L#{n}" } }
+
+      expect { |_, i|
+        MerkleTree.new(*messages[i])
+      }.to perform_linear.in_range(sizes)
     end
 
-    expect { |n, i|
-      MerkleTree.new(*messages[i])
-    }.to perform_linear.in_range(8, 8 << 12)
+    it "allocates 6 objects for an empty merkle tree" do
+      expect {
+        MerkleTree.new
+      }.to perform_allocation(6).objects
+    end
+
+    it "allocates 85 objects for a merkle tree with 8 messages" do
+      messages = 8.times.each_with_object([]) { |i, acc| acc << "L#{i}" }
+
+      expect {
+        MerkleTree.new(*messages)
+      }.to perform_allocation(85).objects
+    end
   end
 
-  it "checks if a message belongs in logarithmic time" do
-    trees = []
-    bench_range(8, 8 << 12).each do |n|
-      messages = []
-      n.times { |i| messages << "L#{i}" }
-      trees << MerkleTree.new(*messages)
-    end
+  context "#include?" do
+    it "checks if a message belongs in logarithmic time" do
+      sizes = bench_range(8, 8 << 12)
+      trees = sizes.map do |n|
+        messages = n.times.each_with_object([]) { |i, acc| acc << "L#{i}" }
+        MerkleTree.new(*messages)
+      end
 
-    expect { |n, i|
-      trees[i].include?("L#{n/2}", n/2)
-    }.to perform_logarithmic.in_range(8, 8 << 12).sample(100).times
+      expect { |n, i|
+        trees[i].include?("L#{n/2}", n/2)
+      }.to perform_logarithmic.in_range(sizes).sample(100).times
+    end
   end
 end
